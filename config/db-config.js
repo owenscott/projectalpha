1606841b6540918f6791c2fe1a2c4281b70b6b53
Owen Scott <owen.m.scott@gmail.com>
2014-01-19 14:30:47 -0500

    added constructors and config

---
+++ config/db-config.js
@@ -0,0 +1,7 @@
+
+//url to access the database (includes db name)
+exports.DB_URL = 'mongodb://localhost:27017/contractCoding';
+
+//collection name for users
+exports.USERS_COLLECTION = 'users';
+
---
+++ route-constructors/collection-post-route.js
@@ -0,0 +1,44 @@
+var dbConfig = require('./../config/db-config.js');
+var mongoClient = require('mongodb').MongoClient;
+
+var CollectionHandler = function(collection, options) {
+
+	var options = options || {};
+
+	return function(request, reply) {
+
+		mongoClient.connect(dbConfig.DB_URL, function(err, db) {
+
+
+		//adds a new object to the db
+			db.collection(collection).insert(request.payload, function (err, data) {
+				if (err) {
+					throw err;
+				}
+				reply(data);
+				db.close();
+			});
+
+
+		});
+
+	};
+};
+
+
+module.exports = function (rootPath, options) {
+
+	var collection = options.collection || {};
+	var validation = options.validation || {};
+
+	return {
+		method: 'POST',
+		path: rootPath,
+		config: {
+			handler: new CollectionHandler(collection),
+			auth: false,
+			validate: validation
+		}
+	};
+
+};

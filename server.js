fcae348002314dcc8c722052ff7b23ab68ee5aec
Owen Scott <owen.m.scott@gmail.com>
2014-01-20 17:31:20 -0500

    replaced 5 route constructors with one

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// var TestRoute = require('./routes/constructors/collection-get-route.js');
+var RestEndpointSet = require('./routes/constructors/endpoints.js');
+
 //lib
 var Hapi = require('hapi');
 
-//routes
-var apiDataRoutes = require('./routes/data-api.js');
-var apiAdminRoutes = require('./routes/admin-api.js');
-var apiContentRoutes = require('./routes/content-api.js');
+// //routes
+// var apiDataRoutes = require('./routes/data-api.js');
+// var apiAdminRoutes = require('./routes/admin-api.js');
+// var apiContentRoutes = require('./routes/content-api.js');
 
 var server = new Hapi.Server('localhost', 8000);
 
@@ -22,22 +25,28 @@ server.pack.require('hapi-auth-cookie', function(err) {
 		isSecure: false
 	});
 
-	server.route(apiContentRoutes);
-	server.route(apiAdminRoutes);
-	server.route(apiDataRoutes);
+	// server.route(apiContentRoutes);
+	// server.route(apiAdminRoutes);
+	// server.route(apiDataRoutes);
 
 
-	var test = function (request, reply) {
-		reply(request.params);
-	};
-	server.route({
-		method: 'GET',
-		path: '/test/{childId}/test/{id}',
-		config: {
-			handler: test,
-			auth: false
-		}
+
+	// var route = new TestRoute('/api', {
+	// 	filters: [{projects:'projectId'}, {contracts:'contractId'}],
+	// 	verb: 'GET',
+	// 	resourceType: 'document',
+	// 	// collection: 'locations',
+	// 	path: '/locations'
+	// })
+
+	var routes = new RestEndpointSet ('/api', {
+		filters: [],
+		path: '/users'
 	});
+
+	server.route(routes);
+
+	console.log(routes);
 
 	// console.log(server.table());
 
